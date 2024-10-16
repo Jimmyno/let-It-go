2f859965cce75f79d49212e0701f5cd6e34bed37
Girolamo <girolamo.simeone@gmail.com>
2024-10-16 07:59:47 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+function showMeditations() {
+  alert("Here are your guided meditations for today.");
+}
+
+function showCheckIn() {
+  alert("How are you feeling today?");
+}
+
+function showTriggers() {
+  alert("List of triggers to avoid today.");
+}
